6f4659849381e7fb36b3878117d539b49f760f2e
SpencerB <74507086+Blunderscout@users.noreply.github.com>
2021-04-17 11:47:02 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function (require, response)
+{
+	//handle incoming request here..
+});
+
+server.listen(5000);
+
+console.log('Node.js webserver at port 500 is running...')
